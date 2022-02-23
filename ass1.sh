b51528c3738dcfaeb15bb8cddcd6298e708f6ce7
Naveen Kumar Selvaraj <naveenkumars@parkisolutions.com>
2022-02-23 07:23:53 +0530

    qus1

--- ass1.sh
+++ ass1.sh
@@ -1,1 +1,16 @@
 echo "day 6 assingment"
+read -p "Enter the number :" num
+	prime=1
+	for((i=2;i<$num;i++))
+	do
+	if(($num%$i==0))
+	then
+	prime=0
+	fi
+	done
+	if(($prime==1))
+	then
+	echo " the num is prime.."
+	else
+	echo "the num is not prime..."
+	fi
